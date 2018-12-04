ad041d797235d4bacd0d0ce97130e3cc89e1a914
Niko Vrdoljak <niko.vrdoljak@hotmail.com>
2018-12-04 18:05:29 +0100

    Inicijalni projekt spreman

--- 9/Autentikacija/Begin/main.py
+++ 9/Autentikacija/Begin/main.py
@@ -1,8 +1,14 @@
-from flask import Flask
+from flask import Flask, request, session, render_template, redirect, url_for, flash, current_app
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.secret_key = 'NEKA_ŽVRLJOTINA'
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
-    return '<h1>Hello World</h1>'
+    return render_template('index.html')
+
+@app.route('/secret')
+def secret():
+    return render_template('secret.html')
