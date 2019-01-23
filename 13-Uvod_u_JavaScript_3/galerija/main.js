737a87b4d468c9bd5ef919bebae701098ade8ad0
Niko Vrdoljak <niko.vrdoljak@hotmail.com>
2019-01-23 11:25:04 +0100

    Dodana galerija u 13

---
+++ 13-Uvod_u_JavaScript_3/galerija/main.js
@@ -0,0 +1,14 @@
+var displayedImage = document.querySelector('.displayed-img');
+var thumbBar = document.querySelector('.thumb-bar');
+
+btn = document.querySelector('button');
+var overlay = document.querySelector('.overlay');
+
+/* prođimo kroz sve slike i prikažimo ih*/
+
+  var newImage = document.createElement('img');
+  newImage.setAttribute('src', xxx);
+  thumbBar.appendChild(newImage);
+
+/* povežimo gumb za potamljenje / posvijetljenje */
+
